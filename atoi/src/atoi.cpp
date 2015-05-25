e0f39ea8b38b6b2241903c2b306a349a6ce6141e
frank <frank@bigBearDebian.wirelesstech>
2015-05-25 11:12:56 -0700

    intial check in

---
+++ atoi/src/atoi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : atoi.cpp
+// Author      : Frank
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
